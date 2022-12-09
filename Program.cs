49bee822627be2a9a4eb18f1521916e816b9a41b
Khokhwan Weangoukost <khim@KHiM.local>
2022-12-09 14:54:17 +0700

    no2

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    public static Stack<char> leftChar = new Stack<char>();
+    public static Stack<char> rightChar = new Stack<char>();
+    static void InputChar()
+    {
+        char inputChar;
+        while(true)
+        {
+            inputChar = char.Parse(Console.ReadLine());
+            if(inputChar == '?')
+            {
+                break;
+            }
+            else
+            {
+                if (inputChar == '(')
+                {
+                    leftChar.Push(inputChar);
+                }
+                else
+                {
+                    rightChar.Push(inputChar);
+                }
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        InputChar();
+        
+        if (leftChar.GetLength() == rightChar.GetLength())
+        {
+            Console.WriteLine("Valid");
+        }
+        else 
+        {
+            Console.WriteLine("Invalid");
+        }
+    }
+}
